06e96789c038944120d7178feac5309bef047b3d
fedebacci <federicobacci1999@gmail.com>
2025-09-20 13:05:49 +0200

    Created Master layout for all pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,41 +1,29 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <title>
-            Movies (DB)
-        </title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+@section('page-title')
+    Home
+@endsection
 
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+@section('page-content')
+    
+    {{-- @dd($movies) --}}
 
-    </head>
-    <body>
-        
-        {{-- @dd($movies) --}}
-
-        <section class="py-5">
-            <div class="container">
-                <div class="row g-3">
-                    @foreach ($movies as $movie)
-                        <div class="col-6 col-sm-4 col-md-3">
-                            <x-movie-card :movie="$movie">
-                                <x-slot:title>{{ $movie["title"] }}</x-slot>
-                                <x-slot:original_title>{{ $movie["original_title"] }}</x-slot>
-                                <x-slot:date>{{ $movie["date"] }}</x-slot>
-                                <x-slot:nationality>{{ $movie["nationality"] }}</x-slot>
-                                <x-slot:vote>{{ $movie["vote"] }}</x-slot>
-                            </x-movie-card>
-                        </div>
-                    @endforeach
-                </div>
+    <section class="py-5">
+        <div class="container">
+            <div class="row g-3">
+                @foreach ($movies as $movie)
+                    <div class="col-6 col-sm-4 col-md-3">
+                        <x-movie-card :movie="$movie">
+                            <x-slot:title>{{ $movie["title"] }}</x-slot>
+                            <x-slot:original_title>{{ $movie["original_title"] }}</x-slot>
+                            <x-slot:date>{{ $movie["date"] }}</x-slot>
+                            <x-slot:nationality>{{ $movie["nationality"] }}</x-slot>
+                            <x-slot:vote>{{ $movie["vote"] }}</x-slot>
+                        </x-movie-card>
+                    </div>
+                @endforeach
             </div>
-        </section>
-
-    </body>
-</html>
+        </div>
+    </section>
+@endsection
